d8007539d4c25c7985dee8f7067bf44a534f7ec3
praveenraj <112162475+R-praveenraj@users.noreply.github.com>
2023-07-19 21:10:29 +0530

    Add files via upload

---
+++ factors-sort.py
@@ -0,0 +1,17 @@
+def factor(num):
+    i=1
+    count=0
+    while i*i <=num:
+        if num%i==0:
+            if num/i==i:
+                count+=1
+            else:
+                count+=2
+        i+=1
+    return count
+def factor_sort(array):
+    array.sort(key=lambda num:(factor(num),num)) 
+    return array
+
+array=list(map(int,input().split()))
+print(factor_sort(array))
